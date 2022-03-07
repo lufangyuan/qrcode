0ce7afd13cc9d7c269c14c124d93f2c2dcfe1197
cooderlfy <lu_chaoyu@sina.com>
2022-03-07 17:32:51 +0800

    0307公司修改

---
+++ src/components/QRTitle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="title">
+    {{title}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'QRTitle',
+  props: {
+    title: {
+      type: String,
+      default: 'Make a QR code'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .title {
+    width: 100%;
+    text-align: center;
+    margin-top: 100px;
+    font-size: 35px;
+    font-weight: 500;
+    color: rgba(0, 0, 0, 0.8);
+  }
+  @media (max-width: 768px) {
+    .title {
+      margin-top: 50px;
+    }
+  }
+</style>
---
+++ src/components/QRfooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="footer">
+    {{footer}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'QRfooter',
+    props: {
+      footer: {
+        type: String,
+        default: '© Copyright 技术支持:卢方远'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .footer {
+    width: 100%;
+    text-align: center;
+    /* position:fixed; */
+    /* left:0;
+    right: 0;
+    bottom: 30px;
+    font-size: 35px;
+    font-weight: 500;
+    color: rgba(0, 0, 0, 0.8); */
+  }
+  @media (max-width: 768px) {
+ 
+  }
+</style>
